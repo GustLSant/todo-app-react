293f3034a60128587c8d9cd7e7c5075bffa260ea
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2023-08-11 03:52:37 -0300

    criacao ErrorPage e correcao fonte HomePage

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,9 +1,18 @@
-function ErroPage() {
+import './ErrorPage.css'
+import { IoListCircle } from "react-icons/io5"
+
+function ErrorPage() {
+
     return (
         <div className="error-page">
-            <h1>Something wrong happened, please go back to www....</h1>
+            <h1>Something went wrong, please return to the <a href="/">Home Page</a></h1>
+
+            <div>
+                <IoListCircle size={'3.5em'} style={{color: 'var(--color-05)'}} />
+                <h1>ToDoApp</h1>
+            </div>
         </div>
     );
 }
 
-export default ErroPage;
+export default ErrorPage;
